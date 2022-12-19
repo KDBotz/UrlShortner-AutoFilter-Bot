b9496a8f55761e3b9a4eaf22529fc0d39945af77
KD Botz <116437604+KDBotz@users.noreply.github.com>
2022-12-19 11:42:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kdbotz/Lucifer.git /Lucifer
+  git clone https://github.com/kdbotz/UrlShortner-AutoFilter-Bot.git /UrlShortner-AutoFilter-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Lucifer
+  git clone $UPSTREAM_REPO /UrlShortner-AutoFilter-Bot
 fi
-cd /Lucifer
+cd /UrlShortner-AutoFilter-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Lucifer...."
 python3 bot.py
