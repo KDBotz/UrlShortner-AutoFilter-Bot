779c93b3dbbf36610cda5dc0e07929416cac4b3c
KD Botz <116437604+KDBotz@users.noreply.github.com>
2022-12-24 10:59:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,6 +18,7 @@ BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
 PORT = environ.get("PORT", "8080")
+TIMEZONE = environ.get("TIMEZONE", "Asia/Kolkata")
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
